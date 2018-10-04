6fd6f8bbbb72c57fc8ba5113e2b445b30fc01e7e
Orzee777 <bradorzolek@gmail.com>
2018-10-04 16:59:37 -0400

    added neuralNetwork class

--- neuralNetwork.h
+++ neuralNetwork.h
@@ -7,19 +7,45 @@
 #define NEURALNETWORK_H
 
 #include <vector>
+#include <fstream>
 #include <cmath>
+
+#include "../Matrix/Matrix.h"
 
 using namespace std;
 
 class NeuralNetwork {
-private:
-    
-    double cost();
-
 public:
     
+    neuralNetwork(vector<int> neurons, double learningRate);
+    neuralNetwork(const char *filePath);
+
+    Matrix<double> computeOutput(vector<double> input);
+    void learn(vector<double> expectedOutput);
+
+    void saveNetworkParameters(const char *filePath);
+    void loadNetworkParameters(const char *filePath);
+
+    vector<Matrix<double> > W;
+    vector<Matrix<double> > B;
+
+private:
+    
+    vector<Matrix<double> > H;
+    vector<Matrix<double> > dEdW;
+    vector<Matrix<double> > dEdB;
+
+    Matrix<double> Y;
+
+    int hiddenLayersCount;
+    double learningRate;
+
+    static double random(double x);
+    static double sigmoid(double x);
+    static double sigmoidPrime(double x);
+
+    void printToFile(Matrix<double> &m, ofstream &file);
     
 };
 
 #endif /* NEURALNETWORK_H */
-
