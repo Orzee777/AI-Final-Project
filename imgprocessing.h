ba28a537acaccaa5fbbaea6c4d8a84f6ac562d32
Orzee777 <37989885+Orzee777@users.noreply.github.com>
2018-10-26 13:25:19 -0400

    include opencv2 files

--- imgprocessing.h
+++ imgprocessing.h
@@ -18,6 +18,10 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/ml.hpp>
+
+#include <iostream>
 
 using namespace cv;
 
